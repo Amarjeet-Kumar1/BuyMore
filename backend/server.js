40fdc6ef0ce44c3739cbe726c4e78a30d7710565
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-12-27 02:04:08 +0530

    controller for user signin

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,38 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'product not found' });
-  }
-});
+app.use('/api/seed', seedRouter);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'product not found' });
-  }
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
-});
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(port, () => {
+      console.log(`serve at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error in connecting database ${err.message}`);
+  });
