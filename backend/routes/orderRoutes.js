4c77cd5516b35b3304dd7bc575521cf417008148
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2023-01-17 02:04:13 +0530

    data for dashboard

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { isAuth } from '../utils.js';
+import { isAdmin, isAuth } from '../utils.js';
 import expressAsyncHandler from 'express-async-handler';
 
 import Order from '../models/orderModel.js';
+import Product from '../models/productModel.js';
+import User from '../models/userModel.js';
 
 const orderRouter = express.Router();
 
@@ -24,6 +26,60 @@ orderRouter.post(
     res.status(201).send({ message: 'New Order Created', order });
   })
 );
+
+orderRouter.get(
+  '/summary',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    //aggregate ops process multiple documents and return computed results
+    //it can be used to, group value from multiple docs, perform ops on groudped data, analyze data change over time
+
+    const orders = await Order.aggregate([
+      {
+        //group data of same id
+        $group: {
+          _id: null,
+          //increase numOrders count for every order
+          numOrders: { $sum: 1 },
+          // add totalPrice to totalSales for every order
+          totalSales: { $sum: '$totalPrice' },
+        },
+      },
+    ]);
+    const users = await User.aggregate([
+      {
+        $group: {
+          _id: null,
+          numUsers: { $num: 1 },
+        },
+      },
+    ]);
+    const dailyOrders = await Order.aggregate([
+      {
+        $group: {
+          //group data of same id, here as date
+          //dateToString convert a date object to a string as specified format
+          _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+          order: { $sum: 1 },
+          sales: { $sum: '$totalPrice' },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+    const productCategories = await Product.aggregate([
+      {
+        $group: {
+          //group data of same category
+          _id: '$category',
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    res.send({ users, dailyOrders, productCategories });
+  })
+);
+
 orderRouter.get(
   '/mine',
   isAuth,
